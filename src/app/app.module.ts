0a73e5c6031e3d093bc897d31f7482baf70a7073
David Villaron <davidvillaron@tech901.org>
2023-02-10 01:39:25 -0600

    added login/signup dialog to nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -7,6 +8,14 @@ import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavigationComponent } from './components/navigation/navigation.component';
+
+//PrimeNG
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {DialogModule} from 'primeng/dialog';
+import {PasswordModule} from 'primeng/password';
+import { DividerModule } from "primeng/divider";
+import {RippleModule} from 'primeng/ripple';
 
 @NgModule({
   declarations: [
@@ -18,7 +27,14 @@ import { NavigationComponent } from './components/navigation/navigation.componen
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    DialogModule,
+    PasswordModule,
+    DividerModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(public http:HttpClient) { }
+
+  //Create
+
+  //Read
+
+  //Update
+
+  //Delete
+
+}
