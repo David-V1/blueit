b9ee876cbbcc456e1fe8645d61dbf83d86b5502e
David Villaron <davidvillaron@tech901.org>
2023-02-10 01:38:46 -0600

    fixed primeng css display. Added nav bar

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  loginData = {} as User;
+  newAccountData = {} as User;
+  searchValue: string = '';
+  displayLogin: boolean = false;
+  displayCreateAccount: boolean = false;
+
+  public constructor(private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+
+  public showBasicDialog() {
+    this.displayLogin = true;
+  }
+
+  public onLogin() {
+    console.log(this.loginData);
+    this.displayLogin = false;
+    this.resetLoginFields();
+  }
+
+  public onSingup() {
+    console.log(this.newAccountData);
+    this.displayCreateAccount = false
+    this.resetLoginFields();
+  }
+
+  public showLogin() {
+    this.displayLogin = true;
+    this.displayCreateAccount = false;
+  }
+
+  public showSignup() {
+    this.displayLogin = false;
+    this.displayCreateAccount = true;
+  }
+
+  public resetLoginFields(){
+    this.loginData = {} as User;
+  }
 
 }
