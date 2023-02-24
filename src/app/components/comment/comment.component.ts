a1670afa77701473cfc3e96b142d9c94a0545a0d
David Villaron <90426008+David-V1@users.noreply.github.com>
2023-02-23 22:23:52 -0600

    POST/GET Comments from DB

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
-import { Comment } from 'src/app/models/Comment';
+import {Comment} from '../../models/Comment'
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-comment',
@@ -8,7 +9,20 @@ import { Comment } from 'src/app/models/Comment';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent {
+  comment = {} as Comment;
 
-  constructor(public ui: UiService) { }
+  constructor(public ui: UiService, public postService: PostService) { }
+
+  public onCommentSubmit(){ 
+    this.postService.addComment(this.comment, this.postService.currentPostId, this.ui.currentUserId!);
+  }
+
+  commentTest() {
+
+    this.postService.getCommentsByPostId(this.postService.currentPostId);
+    this.postService.comment$.subscribe((data) => {
+      console.log(data);
+    });
+  }
 
 }
--- src/app/models/Comment.ts
+++ src/app/models/Comment.ts
@@ -2,9 +2,10 @@ import { Post } from "./Post";
 import { User } from "./User";
 
 export interface Comment {
-  id: number;
+  id: number | null;
   commentText: string;
-  commentDate: Date;
-  user: User;
+  commentDate: null;
   post: Post;
+  user: User;
+  
 }
